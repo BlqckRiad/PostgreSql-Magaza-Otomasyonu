244cb9d39e7c206dea41c9ff0072250948fcab31
Enes Özbuğanlı <99333993+BlqckRiad@users.noreply.github.com>
2022-12-27 22:41:51 +0300

    C#-POSTGRESQL-MAGAZA-OTOMASYONU
    SAÜ VERİ TABANI YÖNETİM SİSTEMLERİ PROJESİ

---
+++ PostgreMagazaProje/Menü.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace PostgreMagazaProje
+{
+    partial class Menü
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Menüden_Urunlere = new System.Windows.Forms.Button();
+            this.Menüden_Kategorilere = new System.Windows.Forms.Button();
+            this.MenüdenCalisana = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.Kampanya = new System.Windows.Forms.Button();
+            this.Menüden_Tedarikçilere = new System.Windows.Forms.Button();
+            this.MenüdenSiparişe = new System.Windows.Forms.Button();
+            this.MenüdenVerilere = new System.Windows.Forms.Button();
+            this.MenüdenMuster = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Menüden_Urunlere
+            // 
+            this.Menüden_Urunlere.Location = new System.Drawing.Point(171, 82);
+            this.Menüden_Urunlere.Name = "Menüden_Urunlere";
+            this.Menüden_Urunlere.Size = new System.Drawing.Size(119, 54);
+            this.Menüden_Urunlere.TabIndex = 0;
+            this.Menüden_Urunlere.Text = "ÜRÜNLER İÇİN";
+            this.Menüden_Urunlere.UseVisualStyleBackColor = true;
+            this.Menüden_Urunlere.Click += new System.EventHandler(this.Menüden_Urunlere_Click);
+            // 
+            // Menüden_Kategorilere
+            // 
+            this.Menüden_Kategorilere.Location = new System.Drawing.Point(387, 82);
+            this.Menüden_Kategorilere.Name = "Menüden_Kategorilere";
+            this.Menüden_Kategorilere.Size = new System.Drawing.Size(119, 54);
+            this.Menüden_Kategorilere.TabIndex = 1;
+            this.Menüden_Kategorilere.Text = "Kategoriler İçin";
+            this.Menüden_Kategorilere.UseVisualStyleBackColor = true;
+            this.Menüden_Kategorilere.Click += new System.EventHandler(this.Menüden_Kategorilere_Click);
+            // 
+            // MenüdenCalisana
+            // 
+            this.MenüdenCalisana.Location = new System.Drawing.Point(171, 182);
+            this.MenüdenCalisana.Name = "MenüdenCalisana";
+            this.MenüdenCalisana.Size = new System.Drawing.Size(119, 54);
+            this.MenüdenCalisana.TabIndex = 2;
+            this.MenüdenCalisana.Text = "Çalışanlar İçin";
+            this.MenüdenCalisana.UseVisualStyleBackColor = true;
+            this.MenüdenCalisana.Click += new System.EventHandler(this.MenüdenCalisana_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(607, 182);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(119, 54);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Araç İşlemleri İçin";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Kampanya
+            // 
+            this.Kampanya.Location = new System.Drawing.Point(387, 182);
+            this.Kampanya.Name = "Kampanya";
+            this.Kampanya.Size = new System.Drawing.Size(119, 54);
+            this.Kampanya.TabIndex = 4;
+            this.Kampanya.Text = "Kampanyalar İçin";
+            this.Kampanya.UseVisualStyleBackColor = true;
+            this.Kampanya.Click += new System.EventHandler(this.Kampanya_Click);
+            // 
+            // Menüden_Tedarikçilere
+            // 
+            this.Menüden_Tedarikçilere.Location = new System.Drawing.Point(607, 82);
+            this.Menüden_Tedarikçilere.Name = "Menüden_Tedarikçilere";
+            this.Menüden_Tedarikçilere.Size = new System.Drawing.Size(119, 54);
+            this.Menüden_Tedarikçilere.TabIndex = 5;
+            this.Menüden_Tedarikçilere.Text = "Tedarikçiler İçin";
+            this.Menüden_Tedarikçilere.UseVisualStyleBackColor = true;
+            this.Menüden_Tedarikçilere.Click += new System.EventHandler(this.Menüden_Tedarikçilere_Click);
+            // 
+            // MenüdenSiparişe
+            // 
+            this.MenüdenSiparişe.Location = new System.Drawing.Point(387, 296);
+            this.MenüdenSiparişe.Name = "MenüdenSiparişe";
+            this.MenüdenSiparişe.Size = new System.Drawing.Size(119, 54);
+            this.MenüdenSiparişe.TabIndex = 8;
+            this.MenüdenSiparişe.Text = "Sipariş İçin";
+            this.MenüdenSiparişe.UseVisualStyleBackColor = true;
+            this.MenüdenSiparişe.Click += new System.EventHandler(this.MenüdenSiparişe_Click);
+            // 
+            // MenüdenVerilere
+            // 
+            this.MenüdenVerilere.Location = new System.Drawing.Point(607, 296);
+            this.MenüdenVerilere.Name = "MenüdenVerilere";
+            this.MenüdenVerilere.Size = new System.Drawing.Size(119, 54);
+            this.MenüdenVerilere.TabIndex = 7;
+            this.MenüdenVerilere.Text = "Şirket Verileri İçin";
+            this.MenüdenVerilere.UseVisualStyleBackColor = true;
+            this.MenüdenVerilere.Click += new System.EventHandler(this.MenüdenVerilere_Click);
+            // 
+            // MenüdenMuster
+            // 
+            this.MenüdenMuster.Location = new System.Drawing.Point(171, 296);
+            this.MenüdenMuster.Name = "MenüdenMuster";
+            this.MenüdenMuster.Size = new System.Drawing.Size(119, 54);
+            this.MenüdenMuster.TabIndex = 6;
+            this.MenüdenMuster.Text = "Müşteriler İçin";
+            this.MenüdenMuster.UseVisualStyleBackColor = true;
+            this.MenüdenMuster.Click += new System.EventHandler(this.MenüdenMuster_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(400, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 29);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "MENÜ";
+            // 
+            // Menü
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MenüdenSiparişe);
+            this.Controls.Add(this.MenüdenVerilere);
+            this.Controls.Add(this.MenüdenMuster);
+            this.Controls.Add(this.Menüden_Tedarikçilere);
+            this.Controls.Add(this.Kampanya);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.MenüdenCalisana);
+            this.Controls.Add(this.Menüden_Kategorilere);
+            this.Controls.Add(this.Menüden_Urunlere);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Name = "Menü";
+            this.Text = "MENÜ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Menüden_Urunlere;
+        private System.Windows.Forms.Button Menüden_Kategorilere;
+        private System.Windows.Forms.Button MenüdenCalisana;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button Kampanya;
+        private System.Windows.Forms.Button Menüden_Tedarikçilere;
+        private System.Windows.Forms.Button MenüdenSiparişe;
+        private System.Windows.Forms.Button MenüdenVerilere;
+        private System.Windows.Forms.Button MenüdenMuster;
+        private System.Windows.Forms.Label label1;
+    }
+}
